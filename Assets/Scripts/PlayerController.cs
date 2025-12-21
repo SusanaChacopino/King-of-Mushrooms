1a681748d6260483cec7ab79c514c7a4a03052c1
SusanaChacopino <chacopino@hotmail.es>
2025-12-22 00:18:10 +0100

    Commit07
    min 2:11:35

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using JetBrains.Annotations;
 using Unity.Netcode;
 using UnityEngine;
@@ -12,6 +13,7 @@ public class PlayerController : NetworkBehaviour
 	private Camera _mainCamera;
 	private Vector3 _mouseInput = Vector3.zero;
 	private PlayerLenght _playerLength;
+	private bool _canCollide = true;
 
 	private readonly ulong[] _targetClientsArray = new ulong[1];
 	
@@ -30,7 +32,36 @@ public class PlayerController : NetworkBehaviour
 	private void Update()
 	{
 		if (!IsOwner || !Application.isFocused) return;
+		MovePlayerServer();
+	}
 
+	private void MovePlayerServer()
+	{
+		_mouseInput.x = Input.mousePosition.x;
+		_mouseInput.y = Input.mousePosition.y;
+		_mouseInput.z = _mainCamera.nearClipPlane;
+		Vector3 mouseWorldCoordinates = _mainCamera.ScreenToWorldPoint(_mouseInput);
+		mouseWorldCoordinates.z = 0f;
+		MovePlayerServerRpc(mouseWorldCoordinates);
+	}
+
+	[ServerRpc]
+	private void MovePlayerServerRpc(Vector3 mouseWorldCoordinates)
+	{
+		transform.position = Vector3.MoveTowards(transform.position,mouseWorldCoordinates,Time.deltaTime * speed);
+
+		//Rotacion
+		if (mouseWorldCoordinates != transform.position)
+		{
+			Vector3	targetDirection = mouseWorldCoordinates - transform.position;
+			targetDirection.z = 0f;
+			transform.up = targetDirection;
+		}
+	}
+
+	//Movimiento del cliente por el mismo
+	private void MovePlayerClient() 
+	{
 		//Movimiento
 		_mouseInput.x = Input.mousePosition.x;
 		_mouseInput.y = Input.mousePosition.y;
@@ -46,6 +77,7 @@ public class PlayerController : NetworkBehaviour
 			targetDirection.z = 0f;
 			transform.up = targetDirection;
 		}
+	
 	}
 
 	[ServerRpc(RequireOwnership = false)]
@@ -95,12 +127,22 @@ public class PlayerController : NetworkBehaviour
 		NetworkManager.Singleton.Shutdown();
 	}
 
+	private IEnumerator CollisionCheckCoroutine()
+	{
+		_canCollide =  false;
+		yield return new WaitForSeconds(0.5f);
+		_canCollide = true;
+	}
+
 	private void OnCollisionEnter2D(Collision2D col)
 	{
 		Debug.Log("Player Collision");
 
 		if(!col.gameObject.CompareTag("Player")) return;
 		if(!IsOwner) return;
+		if(!_canCollide) return;
+
+		StartCoroutine(CollisionCheckCoroutine());
 
 		//Colision de la cabeza
 		if(col.gameObject.TryGetComponent(out PlayerLenght playerLength))
@@ -116,7 +158,7 @@ public class PlayerController : NetworkBehaviour
 			var player2 = new PlayerData()
 			{
 				Id = playerLength.OwnerClientId,
-				Length = _playerLength.length.Value
+				Length = playerLength.length.Value
 			};
 
 			DetermineCollisionWinnerServerRpc(player1, player2);
